53bfe55143df715cb66848d3b396ae0c06d7535c
Samisafool <thenerdiestguy@gmail.com>
2022-08-20 23:27:28 +0530

    Update

--- in-chroot.zsh
+++ in-chroot.zsh
@@ -4,13 +4,15 @@
 
 hwclock --systohc
 
+echo "Choose locale now."
 read -rsk 1
 ln -sf /usr/share/zoneinfo/"$(cd /usr/share/zoneinfo; fzf)" /etc/localtime
 locale-gen
 
 bootctl install
 mkinitcpio -P -c $dir/mkinitcpio
-sbctl bundle -c ./cmd /boot/efi/EFI/Linux/arch.efi
+cp ./cmd /etc/kernel/cmdline
+sbctl bundle -c /etc/kernel/cmdline /boot/efi/EFI/Linux/arch.efi
 
 useradd -mG wheel arch
 passwd arch
