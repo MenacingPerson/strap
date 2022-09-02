124d307a5fca4ddd88df69d4727f454f647082ea
Samisafool <thenerdiestguy@gmail.com>
2022-09-02 08:30:33 +0530

    Update

--- lib/in-chroot.zsh
+++ lib/in-chroot.zsh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-. $(dirname "$0")/common.zsh
+. $(dirname $(realpath "$0"))/common.zsh
 
 echo "Choose locale now."
 read -rsk 1
