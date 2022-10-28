9e7f72212beadd1c3aa45c0cce5d142594c24042
Samisafool <thenerdiestguy@gmail.com>
2022-10-28 17:50:50 +0530

    Update

--- lib/partsetup.zsh
+++ lib/partsetup.zsh
@@ -27,28 +27,41 @@ mm_uefi=($(mm "$1"))
 mm_root=($(mm "$2"))
 mm_home=($(mm "$3"))
 
-echo "==> Setting PARTLABELs"
+echo "==> Configuring partitions:"
+
+echo "===> Setting PARTLABELs:"
 parted /dev/block/$mm_uefi[1]:0 name $mm_uefi[2] LINUX-UEFI
 parted /dev/block/$mm_root[1]:0 name $mm_root[2] LINUX-ROOT
 parted /dev/block/$mm_home[1]:0 name $mm_home[2] LINUX-HOME
 
-echo "==> Setting UEFI partition type to EFI System Partition"
+echo "===> Setting UEFI partition type to EFI System Partition"
 parted /dev/block/$mm_uefi[1]:0 set $mm_uefi[2] esp on
 
-echo "==> Creating FAT32 partition for $1:"
+echo "===> Creating FAT32 partition for $1:"
 mkfs.fat -F32 -n LINUX-UEFI "$1"
 
-echo "==> Creating LUKS container 'root' for $2:"
+echo "===> Creating LUKS container 'root' for $2:"
 cryptsetup luksFormat --label LINUX-ROOT "$2" -q <<< "$PASS"
 cryptsetup open "$2" root <<< "$PASS"
 mkfs.btrfs --label LINUX-ROOT-DECRYPT /dev/mapper/root
 
-echo "==> Creating LUKS container 'home' for $3:"
+echo "===> Creating LUKS container 'home' for $3:"
 cryptsetup luksFormat --label LINUX-HOME "$3" -q <<< "$PASS"
 cryptsetup open "$3" home <<< "$PASS"
 mkfs.btrfs --label LINUX-HOME-DECRYPT /dev/mapper/home
 
 echo "==> Mounting and creating subvolumes:"
+
+echo "===> Mounting and configuring $2 (root):"
+mount /dev/mapper/root /mnt -o compress=zstd:1
+for i in @ @varlog @varcache
+do
+    btrfs subvolume create /mnt/$i
+done
+umount /mnt
+mount /dev/mapper/root /mnt -o compress=zstd:1,subvol=@
+mount /dev/mapper/root /mnt/var/log -o compress=zstd:1,subvol=@varlog
+mount /dev/mapper/root /mnt/var/cache -o compress=zstd:1,subvol=@varcache
 
 echo "===> Creating necessary folders:"
 mkdir -p /mnt/boot/efi
@@ -58,20 +71,7 @@ mkdir -p /mnt/var/{log,cache}
 echo "===> Mounting $1 (uefi):"
 mount "$1" /mnt/boot/efi
 
-echo "===> Mounting and configuring $2 (root):"
-mount /dev/mapper/root /mnt -o compress=zstd:1
-
-for i in @ @varlog @varcache
-do
-    btrfs subvolume create /mnt/$i
-done
-
-umount /mnt
-mount /dev/mapper/root /mnt -o compress=zstd:1,subvol=@
-
-mount /dev/mapper/root /mnt/var/log -o compress=zstd:1,subvol=@varlog
-mount /dev/mapper/root /mnt/var/cache -o compress=zstd:1,subvol=@varcache
-
+echo "===> Mounting $3 (home):"
 mount /dev/mapper/home /mnt/home -o compress=zstd:1
 btrfs subvolume create /mnt/home/@home
 umount /mnt/home
